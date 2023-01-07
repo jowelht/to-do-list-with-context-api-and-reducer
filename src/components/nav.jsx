c899f31fdf40c136e443dbad379b0cd7af25e6a5
jowelht <jowel@hastechit.com>
2023-01-07 18:10:54 +0600

    add active class in react router nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Menu = () => {
   return (
     <ul className="nav">
-        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/home" className={(navInfo)=> navInfo.isActive ? `active`: ''}>Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/contact">Contact</NavLink></li>
     </ul>
